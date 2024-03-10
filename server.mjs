69c030e1b28c2329726dfc0ade1304c4146b2bce
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-10 10:03:59 -0400

    connected to db

--- server.mjs
+++ server.mjs
@@ -1,8 +1,22 @@
 import express from "express";
 const app = express();
 
+import { createClient } from "@libsql/client";
+const client = createClient({
+  url: "libsql://monthly-budget-slandath.turso.io",
+  authToken:
+    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MTAwNzc2MTcsImlkIjoiNmJkNmQxNjAtM2MwYy00YTM0LThjMmEtNGM3ZDEzNTdlM2I2In0.kazdf7O56ybrWf35MUcMJBBc4VO-gPXBw1vKtozMzZOhfJdNHvBJ_OjHp-2eF7RPQfNDvRtIi5QzxiX_EmwCAw",
+});
+const result = await client.execute(
+  "SELECT category, description, amount FROM transactions WHERE ID = 1",
+);
+
 app.get("/", (req, res) => {
   res.send("Welcome to Tom's Web Server");
+});
+
+app.get("/db", (req, res) => {
+  res.json(result);
 });
 
 app.listen(process.env.PORT, () =>
