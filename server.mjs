8653caf0a411b4563b9b007c6ecc13c9909c888c
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-10 09:27:05 -0400

    added new message

--- server.mjs
+++ server.mjs
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Welcome to Tom's Web Server");
 });
 
 app.listen(process.env.PORT, () =>
