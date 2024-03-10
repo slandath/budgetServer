61102989415cba96729cbbaf50919cc60cffeedf
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-10 08:59:53 -0400

    added env

---
+++ server.mjs
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Server is listening on port ${process.env.PORT}`),
+);
