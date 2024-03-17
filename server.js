c94966700a4f86bfbc68fdb5064c23a856c5c861
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-17 16:16:37 -0400

    trying with variables

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.get('/sql', async (req, res) => {
 
 app.post('/add', async(req, res) => {
   console.log(req.body.category, req.body.description, req.body.amount)
-  const sql = "INSERT INTO budget (category, description, amount) VALUES ('expense', 'internet', 65)"
+  const sql = `INSERT INTO budget (category, description, amount) VALUES ('${req.body.category}', '${req.body.description}', ${req.body.amount})`
   const query = await executeQuery(sql)
   res.json(query.rows)
 })
