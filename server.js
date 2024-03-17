ae363141cace402b77420c386992d33f4c0faee6
Tom Slanda <tomslanda@Toms-MacBook-Air.local>
2024-03-17 16:33:13 -0400

    cleanup

--- server.js
+++ server.js
@@ -79,6 +79,5 @@ app.get('/sql', async (req, res) => {
 app.post('/add', async(req, res) => {
   console.log(req.body.category, req.body.description, req.body.amount)
   const sql = `INSERT INTO budget (category, description, amount) VALUES ('${req.body.category}', '${req.body.description}', ${req.body.amount})`
-  const query = await executeQuery(sql)
-  res.json(query.rows)
+  await executeQuery(sql)
 })
